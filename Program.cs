5134d4fcf78131a906d865f5fa236545559b2df3
nevenall <nevenall@hotmail.com>
2017-02-11 10:32:43 -0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace spellCheckTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
