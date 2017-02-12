862df15974a38aa93c2dc8c19ca56af2f7eb8efb
nevenall <nevenall@hotmail.com>
2017-02-12 12:43:24 -0800

    functional spell checking from a file path or from input stream

--- Program.cs
+++ Program.cs
@@ -1,15 +1,71 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using PlatformSpellCheck;
 
 namespace SpellCheckTask
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-           Console.WriteLine("hello");
-        
-Console.ReadLine();
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				using (var spelling = new SpellChecker())
+				{
 
-        }
-    }
+					var lineNo = 0;
+					IEnumerable<string> lines = Enumerable.Empty<string>();
+
+					if (Console.IsInputRedirected)
+					{
+						lines = ReadFromInput();
+					}
+					else if (args.Length >= 1 && !string.IsNullOrWhiteSpace(args[0]) && File.Exists(args[0]))
+					{
+						lines = File.ReadLines(args[0]);
+					}
+					else
+					{
+						System.Console.WriteLine(@"Usage: 
+
+ <Your Input> | SpellCheckTask
+	OR
+ SpellCheckTask ""Path to File to Check""");
+					}
+
+					var query = from line in lines
+									let currentLine = ++lineNo
+									where !string.IsNullOrWhiteSpace(line)
+									from error in spelling.Check(line)
+									where error.RecommendedAction == RecommendedAction.GetSuggestions
+									let misspelling = line.Substring((int)error.StartIndex, (int)error.Length)
+									let suggestions = string.Join(", ", spelling.Suggestions(misspelling))
+									select $"{currentLine}:{error.StartIndex + 1} {misspelling} -> {suggestions}";
+
+					foreach (var el in query)
+					{
+						System.Console.WriteLine(el);
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				System.Console.WriteLine(ex.ToString());
+			}
+		}
+
+		private static IEnumerable<string> ReadFromInput()
+		{
+			using (var lstream = new StreamReader(Console.OpenStandardInput()))
+			{
+				var line = string.Empty;
+				while ((line = lstream.ReadLine()) != null)
+				{
+					yield return line;
+				}
+			}
+		}
+	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # SpellCheckTask
+
+A console app in .net 4.6.2 to spell check text using the Windows Spell Checking API. 
+
+## Usage
+	
+```
+SpellCheckTask 'file to check' 
+```
+
+_or_
+
+```
+<Your input> | SpellCheckTask
+```
+
+## Output
+
+If misspellings are found, each will be output on a separate line. 
+
+```
+[line number]:[column] [misspelled word] -> [comma separated list of suggestions]
+```
